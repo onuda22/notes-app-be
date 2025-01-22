34963753f13cae5a2776be3f46b8f799fc629a0e
onuda22 <okinabada@gmail.com>
2025-01-22 22:23:19 +0700

    feat: register notes plugin on Hapi server

--- src/server.js
+++ src/server.js
@@ -1,7 +1,13 @@
 const Hapi = require('@hapi/hapi');
-const routes = require('./api/notes/routes');
+const notes = require('./api/notes');
+const NotesService = require('./services/inMemory/NotesService');
 
 const init = async () => {
+  /**
+   * V1.0.0
+   * deprecated
+   *
+   *
   const server = Hapi.server({
     port: 3000,
     // eslint-disable-next-line no-undef
@@ -17,6 +23,33 @@ const init = async () => {
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
+  */
+
+  /**
+   * V2.0.0
+   */
+  const notesService = new NotesService();
+
+  const server = Hapi.Server({
+    port: 3000,
+    // eslint-disable-next-line no-undef
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+
+  await server.register({
+    plugin: notes,
+    options: {
+      service: notesService,
+    },
+  });
+
+  await server.start();
+  console.log(`Server running on ${server.info.uri}`);
 };
 
 init();
