bab4e38148a5cd0db55e14bf9746de1f26a75060
onuda22 <okinabada@gmail.com>
2025-01-29 00:00:16 +0700

    feat: apply environment

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const Hapi = require('@hapi/hapi');
 const notes = require('./api/notes');
 const NotesService = require('./services/inMemory/NotesService');
@@ -33,9 +35,10 @@ const init = async () => {
   const notesService = new NotesService();
 
   const server = Hapi.Server({
-    port: 3000,
     // eslint-disable-next-line no-undef
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    port: process.env.PORT,
+    // eslint-disable-next-line no-undef
+    host: process.env.HOST,
     routes: {
       cors: {
         origin: ['*'],
