f577a4dad6b9287f5305215b734f7fcb870e8a70
onuda22 <okinabada@gmail.com>
2025-02-28 15:48:53 +0700

    refactor(service): add username owner to output notes

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,12 +1,21 @@
 /* eslint-disable camelcase */
 // DTO is for get data so that the data issued has a consistent form
-const mapDBToModel = ({ id, title, body, tags, created_at, updated_at }) => ({
+const mapDBToModel = ({
+  id,
+  title,
+  body,
+  tags,
+  created_at,
+  updated_at,
+  username,
+}) => ({
   id,
   title,
   body,
   tags,
   createdAt: created_at,
   updatedAt: updated_at,
+  username,
 });
 
 module.exports = { mapDBToModel };
