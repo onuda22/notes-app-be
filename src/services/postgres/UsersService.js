bccd6dbc4b026ca06cea4e85feb7624b7f413f80
onuda22 <okinabada@gmail.com>
2025-02-28 16:24:34 +0700

    feat: implement query search by username for users

--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -93,6 +93,17 @@ class UsersService {
 
     return id;
   }
+
+  async getUsersByUsername(username) {
+    const query = {
+      text: 'SELECT id, username, fullname FROM users WHERE username LIKE $1',
+      values: [`%${username}%`],
+    };
+
+    const result = await this._pool.query(query);
+    console.log(result.rows);
+    return result.rows;
+  }
 }
 
 module.exports = UsersService;
